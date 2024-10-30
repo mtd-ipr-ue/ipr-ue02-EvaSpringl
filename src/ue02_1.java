3566c7b4e7bd0f61f3516ce0fbdc2c803e301f14
EvaSpringl <evaspringl0@gmail.com>
2024-10-30 12:33:03 +0100

    task 1 and 2 done

--- src/ue02_1.java
+++ src/ue02_1.java
@@ -1,7 +1,31 @@
-import libraries.Out;
+import java.util.Scanner;
 
 public class ue02_1 {
     public static void main(String[] args) {
-        Out.println("Hello, World!");
+        try (Scanner scanner = new Scanner(System.in)) {
+
+            // User input of the three numbers
+            System.out.print("Please enter the first number: ");
+            int number1 = scanner.nextInt();
+
+            System.out.print("Please enter the second number: ");
+            int number2 = scanner.nextInt();
+
+            System.out.print("Please enter the third number: ");
+            int number3 = scanner.nextInt();
+
+            // loop to determine the biggest number
+            int maxValue = number1;
+
+            if (number2 > maxValue) {
+                maxValue = number2;
+            }
+            if (number3 > maxValue) {
+                maxValue = number3;
+            }
+
+            System.out.println("The biggest number is: " + maxValue);
+        }
+
     }
 }
--- src/ue02_2.java
+++ src/ue02_2.java
@@ -1,7 +1,55 @@
-import libraries.Out;
+import java.util.Scanner;
 
 public class ue02_2 {
     public static void main(String[] args) {
-        Out.println("Hello, World!");
+        try (Scanner scanner = new Scanner(System.in)) {
+
+            // User input for year
+            System.out.println("Please enter the year:");
+            int year = scanner.nextInt();
+            // User input for month
+            System.out.println("Please enter a value for the month between 1 and 12:");
+            int month = scanner.nextInt();
+            // User input for days
+            int days = daysInMonth(month, year);
+
+            if (days == 0) {
+                System.out.println("Invalid month!");
+            } else {
+                System.out.println("The Month " + month + " in the year " + year + " has " + days + " days.");
+            }
+        }
+    }
+
+    public static int daysInMonth(int month, int year) {
+        int days;
+        switch (month) {
+            case 1: // january
+            case 3: // march
+            case 5: // may
+            case 7: // july
+            case 8: // august
+            case 10: // october
+            case 12: // december
+                days = 31;
+                break;
+            case 4: // april
+            case 6: // june
+            case 9: // september
+            case 11: // november
+                days = 30;
+                break;
+            case 2: // february
+                if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+                    days = 29; // leap year
+                } else {
+                    days = 28;
+                }
+                break;
+            default:
+                days = 0; // invalid month
+                break;
+        }
+        return days;
     }
 }
